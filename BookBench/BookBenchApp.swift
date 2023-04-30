b391abe5d46e4c96857e51799adc75074bbe5802
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-04-30 10:47:54 +0330

    Initial Commit

---
+++ BookBench/BookBenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookBenchApp.swift
+//  BookBench
+//
+//  Created by Mehdi Karami on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BookBenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
