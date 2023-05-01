bcbac6382cac6b9391907ca4e6d622184c967f29
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-01 05:21:44 +0330

    Authentication View Added.

---
+++ BookBench/AuthenticationView.swift
@@ -0,0 +1,57 @@
+//
+//  AuthenticationView.swift
+//  BookBench
+//
+//  Created by Mehdi Karami on 4/30/23.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    @Environment(\.colorScheme) private var colorScheme
+    @StateObject var authFlowState = AuthFlowState()
+    
+    @State var start = UnitPoint(x: -0.5, y: -2)
+    @State var end = UnitPoint(x: 4, y: 0)
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .default).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: colorScheme == .dark ?
+                                              [.indigo, .black, .purple,
+                                                .black, .indigo, .black]
+                                              :
+                                                [.indigo, .white, .purple,
+                                                 .white, .indigo, .white]
+                                                ), startPoint: start, endPoint: end)
+            .animation(Animation.easeInOut(duration: 30)
+                .repeatForever(autoreverses: true).speed(2), value: start)
+            .animation(Animation.easeInOut(duration: 30)
+                .repeatForever(autoreverses: true).speed(2), value: end)
+            .onReceive(timer, perform: { _ in
+                self.start = UnitPoint(x: 4, y: 0)
+                self.end = UnitPoint(x: 0, y: 2)
+                self.start = UnitPoint(x: -4, y: 20)
+                self.start = UnitPoint(x: 4, y: 0)
+            })
+            .ignoresSafeArea()
+                                        
+            switch authFlowState.currentPage {
+            case .logIn:
+                LogInView()
+            case .signUp:
+                SignUpView()
+            case .forgotPassword:
+                ForgotPasswordView()
+            }
+        }
+    }
+}
+
+
+struct AuthenticationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenticationView()
+    }
+}
