ecde2bb9ca09d99c8fbc27be57f4a1e25a380695
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-01 05:18:33 +0330

    Gradient Icon Added.

---
+++ BookBench/GradientIcon.swift
@@ -0,0 +1,33 @@
+//
+//  GradientIcon.swift
+//  BookBench
+//
+//  Created by Mehdi Karami on 4/30/23.
+//
+
+import SwiftUI
+
+struct GradientIcon: View {
+    @Environment(\.colorScheme) private var colorScheme
+    var systemName: String
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .indigo, .purple]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .cornerRadius(15)
+            
+            Image(systemName: systemName)
+                .font(.title2)
+                .foregroundColor(colorScheme == .dark ? .black : .white)
+        }
+        .frame(width: 50, height: 50)
+    }
+}
+
+struct GradientIcon_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientIcon(systemName: "tree")
+    }
+}
