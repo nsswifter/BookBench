1266e8e019112bdd7dd771ab054b45889bd8109a
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-01 05:22:54 +0330

    Forgot Password View Added.

---
+++ BookBench/ForgotPasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  ForgotPasswordView.swift
+//  BookBench
+//
+//  Created by Mehdi Karami on 4/30/23.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text("ForgotPasswordView")
+    }
+}
+
+struct ForgotPasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForgotPasswordView()
+    }
+}
