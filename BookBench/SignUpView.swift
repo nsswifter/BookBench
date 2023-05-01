dd1fd654fac91c3d9bec7364b1e4796349910cc9
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-01 05:14:47 +0330

    SignUp View Added

---
+++ BookBench/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  BookBench
+//
+//  Created by Mehdi Karami on 4/30/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text("SignInView")
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
