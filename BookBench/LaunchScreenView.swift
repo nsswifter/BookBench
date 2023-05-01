65f70e685afafdcc5bad393b575dae704926696e
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-01 05:16:22 +0330

    Launch Screen View Added

---
+++ BookBench/LaunchScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchScreenView.swift
+//  BookBench
+//
+//  Created by Mehdi Karami on 4/30/23.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        LottieView(name: "BookBenchAnimation")
+    }
+}
+
+struct LaunchScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreenView()
+    }
+}
