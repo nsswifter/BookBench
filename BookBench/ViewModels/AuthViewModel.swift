e4fd152a20cb5f452e55560f8f2a763bf0e47a32
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-14 21:19:50 +0330

    Firebase Firestore imported.

--- BookBench/ViewModels/AuthViewModel.swift
+++ BookBench/ViewModels/AuthViewModel.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestore
+import Security
 
 class AuthViewModel: ObservableObject {
     @Published var currentPage: AuthPage = .logIn
@@ -15,13 +18,47 @@ class AuthViewModel: ObservableObject {
     @Published var email = ""
     @Published var password = ""
     @Published var repeatedPassword = ""
-
+    
     func logIn() {
-        
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+            guard let error else {
+                print("success")
+                return
+            }
+            
+            print(error.localizedDescription)
+        }
     }
     
     func signUp() {
+        if password == repeatedPassword {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                guard let user = authResult?.user, error == nil else {
+                    print("Error creating user: \(error!.localizedDescription)")
+                    return
+                }
+                
+                saveAdditionalUserInfo(for: user)
+            }
+        } else {
+            print("passwords is not matchs")
+        }
         
+        func saveAdditionalUserInfo(for user: User) {
+            let dataBase = Firestore.firestore()
+            
+            dataBase.collection("users").document(user.uid).setData([
+                "firstname": firstname,
+                "lastname": lastname,
+                "email": email
+            ]) { error in
+                if let error = error {
+                    print("Error saving user info: \(error.localizedDescription)")
+                } else {
+                    print("User info saved successfully!")
+                }
+            }
+        }
     }
     
     func forgotPassword() {
