a776f46b22aed575c841f17632cd6f55b5d946bc
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-15 01:33:51 +0330

    Profile View Added.

---
+++ BookBench/Views/ProfileView.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileView.swift
+//  BookBench
+//
+//  Created by Mehdi Karami on 5/14/23.
+//
+
+import SwiftUI
+
+// MARK: - Profile View
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+// MARK: - Profile View Preview
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
