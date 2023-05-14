6e2fbe30bb4f019227372e545fc4ef1831d3dd03
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-14 21:19:05 +0330

    Main View Added.

---
+++ BookBench/Views/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  BookBench
+//
+//  Created by Mehdi Karami on 5/8/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
