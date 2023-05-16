31bccd560b7f64545100871d9af7c0cd17e3eef7
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-16 16:35:14 +0330

    Some Project Modify.

--- BookBench/ViewModels/AuthViewModel.swift
+++ BookBench/ViewModels/AuthViewModel.swift
@@ -8,7 +8,6 @@
 import Foundation
 import Firebase
 import FirebaseFirestore
-import GoogleSignIn
 
 // MARK: - Auth View Model
 
@@ -121,46 +120,6 @@ class AuthViewModel: UIViewController, ObservableObject {
             }
         }
         mode = localMode
-    }
-    
-    func signInGoogle() {
-        guard let clientID = FirebaseApp.app()?.options.clientID else {
-            // Firebase client ID not available
-            return
-        }
-
-        // Create Google Sign In configuration object
-        let config = GIDConfiguration(clientID: clientID)
-        GIDSignIn.sharedInstance.configuration = config
-
-        // Start the sign in flow
-        GIDSignIn.sharedInstance.signIn(withPresenting: self) { result, error in
-            if let error = error {
-                // Handle sign in error
-                print("Sign in with Google failed: \(error.localizedDescription)")
-                return
-            }
-
-            guard let user = result?.user,
-                  let idToken = user.idToken?.tokenString else {
-                // Required user data not available
-                return
-            }
-
-            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
-                                                           accessToken: user.accessToken.tokenString)
-
-            Auth.auth().signIn(with: credential) { result, error in
-                if let error = error {
-                    // Handle Firebase Authentication sign in error
-                    print("Firebase Authentication sign in failed: \(error.localizedDescription)")
-                    return
-                }
-
-                // User is signed in with Firebase Authentication
-                print("User signed in successfully")
-            }
-        }
     }
 }
 
