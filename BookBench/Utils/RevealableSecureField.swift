b411c6a4e4671876173634ee090566dad66188ce
Mehdi Karami <90229452+nsswifter@users.noreply.github.com>
2024-04-22 00:03:45 +0330

    Update Revealable Secure Field

--- BookBench/Utils/RevealableSecureField.swift
+++ BookBench/Utils/RevealableSecureField.swift
@@ -11,21 +11,14 @@ import SwiftUI
 
 /// A SwiftUI view that combines a `SecureField` with a reveal/hide password toggle button.
 struct RevealableSecureField: View {
-    
-    // A focus state for the reveal password mode.
-    @FocusState private var show: Bool
-    
-    // A focus state for the hide password mode.
-    @FocusState private var hide: Bool
-    
     // A Boolean value that determines whether the password is visible or not.
-    @State var showMode = false
+    @State private var showMode = false
     
     // The title of the secure field.
-    var title: String
+    private var title: String
     
     // A binding to the text displayed in the secure field.
-    @Binding var text: String
+    @Binding private var text: String
     
     init(_ title: String, text: Binding<String>) {
         self.title = title
@@ -34,26 +27,22 @@ struct RevealableSecureField: View {
     
     var body: some View {
         HStack {
-            ZStack(alignment: .trailing) {
-                TextField(title, text: $text)
-                    .textContentType(.password)
-                    .focused($show)
-                    .opacity(showMode ? 1 : 0)
-                
-                SecureField(title, text: $text)
-                    .textContentType(.password)
-                    .focused($hide)
-                    .opacity(showMode ? 0 : 1)
-                
-                Button {
-                    showMode.toggle()
-                    if showMode { show = true }
-                    else { hide = true }
-                } label: {
-                    Image(systemName: showMode ? "eye.slash.fill" : "eye.fill")
-                        .font(.system(size: 13, weight: .regular))
-                        .foregroundColor(.accentColor)
+            Group {
+                if showMode {
+                    TextField(title, text: $text)
+                } else {
+                    SecureField(title, text: $text)
                 }
+            }
+            .textContentType(.password)
+            
+            Button {
+                showMode.toggle()
+            } label: {
+                Image(systemName: showMode ? "eye.slash.fill" : "eye.fill")
+                    .foregroundColor(.accentColor)
+                    .bold()
+                    .contentTransition(.symbolEffect(.replace))
             }
         }
     }
@@ -61,8 +50,6 @@ struct RevealableSecureField: View {
 
 // MARK: - Revealable Secure Field View Preview
 
-struct RevealableSecureField_Previews: PreviewProvider {
-    static var previews: some View {
-        AuthView(authViewModel: AuthViewModel())
-    }
+#Preview {
+    AuthView(authViewModel: AuthViewModel())
 }
